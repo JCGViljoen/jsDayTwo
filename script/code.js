589a5946dc82741ef4ca5c2331d50cdcc83015ec
JCGViljoen <jcgviljone51@gmail.com>
2023-05-16 14:35:42 +0200

    js exercise

---
+++ script/code.js
@@ -0,0 +1,7 @@
+let subject = ['Python','C++','VB.Net','Javascript','Assembly' ];
+console.log(subject);
+let lastElement = subject.pop(4);
+console.log(lastElement);
+
+let middleElement = subject[Math.round((subject.length - 1) / 2)];
+console.log(middleElement);
